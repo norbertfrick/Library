c40bae1b35aaa4d8806ac5714e721aeb1b14801f
Norbert Frick <norbert.frick.dev@itvkurze.sk>
2022-06-22 17:50:36 +0200

    Migrations

---
+++ Library.Infrastructure/Migrations/20220622150044_2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TitleType",
+                table: "RentalEntries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeAdded",
+                table: "QueueItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleType",
+                table: "RentalEntries");
+
+            migrationBuilder.DropColumn(
+                name: "TimeAdded",
+                table: "QueueItems");
+        }
+    }
+}
